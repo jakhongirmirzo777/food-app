4b69e9b5d29a98b8a620f94211d9dd9173d5d566
J.Tursunaliev <jakhongirmirzo.tursunaliev@nbrown.co.uk>
2024-03-26 22:33:25 +0500

    Added layout

---
+++ components/ui/VCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="v-card" :style="cardStyle">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VCard',
+  props: {
+    radius: {
+      type: String,
+      default: '0px',
+    },
+    padding: {
+      type: String,
+      default: '0px',
+    },
+    backgroundColor: {
+      type: String,
+      default: 'var(--color-white)',
+    },
+  },
+  computed: {
+    cardStyle() {
+      return {
+        borderRadius: this.radius,
+        padding: this.padding,
+        backgroundColor: this.backgroundColor,
+      }
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import '../../assets/styles/ui/v-card';
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="layout__wrapper">
+    <VCard background-color="var(--color-red)">
+      <div class="d-flex justify-center pa-16">
+        <NuxtLink to="/">
+          <img class="layout__logo" src="/logo.jpeg" alt="logo" />
+        </NuxtLink>
+      </div>
+      <VCard
+        class="layout__body"
+        radius="24px 24px 0 0"
+        padding="24px 16px 0 16px"
+      >
+        <Nuxt />
+      </VCard>
+    </VCard>
+  </div>
+</template>
+
+<script>
+import VCard from '~/components/ui/VCard.vue'
+
+export default {
+  name: 'DefaultLayout',
+  components: { VCard },
+}
+</script>
+
+<style scoped lang="scss">
+@import '../assets/styles/layouts/default.scss';
+</style>
