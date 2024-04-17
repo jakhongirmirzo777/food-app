cb39bbd0183ebf3d53c3120311bb2a014922ef91
J.Tursunaliev <jakhongirmirzo.tursunaliev@nbrown.co.uk>
2024-04-17 22:46:29 +0500

    Fixed meta tags

--- admin/src/pages/_app.js
+++ admin/src/pages/_app.js
@@ -58,12 +58,9 @@ const App = props => {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>{`${themeConfig.templateName} - Material Design React Admin Template`}</title>
-        <meta
-          name='description'
-          content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
-        />
-        <meta name='keywords' content='Material Design, MUI, Admin Template, React Admin Template' />
+        <title>Yalla cafe | yalla-cafe.uz</title>
+        <meta name='description' content={'Mazali taomlar'} />
+        <meta name='keywords' content='burger, chizburgerlar, hot dog, fast food, mazali taomlar' />
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
 
